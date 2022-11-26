cb7ecd3fb7c4c6e34d19c11e730600960f061fff
suprunyuk-a <chaserenergy@gmail.com>
2022-11-26 10:52:25 +0200

    fix node

--- src/components/NotFound/NotFound.styled.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Wrap = styled.div`
-  font-size: 30px;
-  text-align: center;
-  display: block;
-  margin-top: 20px;
-  font-weight: 700;
-`;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import { default as React } from 'react';
+import { default as ReactDOM } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
